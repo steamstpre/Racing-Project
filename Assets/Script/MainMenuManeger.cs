98ece2a891179927b1186ae1bdb409645ac51a2a
denial <denialwins6@gmail.com>
2017-06-26 19:51:18 +0300

    Racing Project

---
+++ Assets/Script/MainMenuManeger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManeger : MonoBehaviour {
+
+    public GameObject Panel;
+    
+
+
+    public void GamePlay()
+
+    {
+        SceneManager.LoadScene ("Game_Scena");
+    }
+   
+    public void Settings()
+    {
+
+    }
+
+  
+    public void About()
+    {
+
+        Panel.SetActive(true);
+    }
+}
---
+++ Assets/Script/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    private Rigidbody _rigidbody;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        _rigidbody.AddForce(direction * speed);
+    }
+}
